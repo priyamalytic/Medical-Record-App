5fb798c76b8264021f5f63d5c21a828bf817ea23
PRIYAM <priyam266german@gmail.com>
2021-02-27 16:05:44 +0530

    one icon removed in app bar

--- lib/components/sideAppbarIcons.dart
+++ lib/components/sideAppbarIcons.dart
@@ -6,23 +6,6 @@ import '../constants.dart';
 List<Widget> SideIcons() {
   List<Widget> sideIcons = [
     Padding(
-        padding: EdgeInsets.only(right: 15.0),
-        child: Container(
-          height: getProportionateScreenHeight(45),
-          width: getProportionateScreenWidth(45),
-          decoration: BoxDecoration(
-            color: kSecondaryColor.withOpacity(0.1),
-            shape: BoxShape.circle,
-          ),
-          child: GestureDetector(
-            onTap: () {},
-            child: Icon(
-              Icons.add,
-              size: 26.0,
-            ),
-          ),
-        )),
-    Padding(
       padding: EdgeInsets.only(right: 15.0),
       child: Container(
         height: getProportionateScreenHeight(45),
