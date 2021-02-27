b62ba69c0233c3b780fd08d9ea0bd0186fbd1151
PRIYAM <priyam266german@gmail.com>
2021-02-27 19:17:51 +0530

    inside changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -31,6 +31,7 @@ const String kMatchPassError = "Passwords don't match";
 const String kNamelNullError = "Please Enter your name";
 const String kPhoneNumberNullError = "Please Enter your phone number";
 const String kAddressNullError = "Please Enter your address";
+const String kPatientIdNullError = "Please Enter Patient Id";
 
 final otpInputDecoration = InputDecoration(
   contentPadding:
--- lib/screens/add_record/component/body.dart
+++ lib/screens/add_record/component/body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:med_app/components/custom_surfix_icon.dart';
+import 'package:med_app/components/default_button.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -6,11 +11,84 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+  String patientId;
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Add Patient record."),
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: getProportionateScreenHeight(150)),
+                Text(
+                  "Add Patient record.",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: getProportionateScreenHeight(40)),
+                buildPatientIdFormField(),
+                SizedBox(height: getProportionateScreenHeight(40)),
+                DefaultButton(
+                  text: "Continue",
+                  press: () {
+                    if (_formKey.currentState.validate()) {
+                      // next page
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildPatientIdFormField() {
+    return TextFormField(
+      onSaved: (newValue) => patientId = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: kPatientIdNullError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kPatientIdNullError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Patient Id",
+        hintText: "Enter Patient Id",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/User.svg"),
       ),
     );
   }
